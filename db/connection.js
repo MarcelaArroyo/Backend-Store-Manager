b8241efe482d62dca91ff356a0f94d9757a6244b
Marcela Arroyo <malvesarroyo@gmail.com>
2022-05-31 17:27:22 -0300

    fix: changing host to localhost in connection file

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
